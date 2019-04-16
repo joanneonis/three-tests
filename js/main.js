1fd7ba8f2463314c0db5534e3f56e8f99e1d455f
joanne <joanneonis@gmail.com>
2019-04-16 10:09:30 +0200

    made index

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import * as THREE from 'three';
-import 'three/examples/js/controls/OrbitControls';
-import 'three/examples/js/exporters/OBJExporter';
-import 'three/examples/js/controls/TrackballControls.js';
+// import * as THREE from 'three';
+// import 'three/examples/js/controls/OrbitControls';
+// import 'three/examples/js/exporters/OBJExporter';
+// import 'three/examples/js/controls/TrackballControls.js';
 
 // start the magic here
 console.log('hellooo');
