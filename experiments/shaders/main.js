5b4bcbbd0513710e6b2f848d2a3d04e42da8a096
joanne <joanneonis@gmail.com>
2019-04-23 18:47:02 +0200

    base

--- experiments/shaders/main.js
+++ experiments/shaders/main.js
@@ -16,19 +16,64 @@ let renderer,
 
 var gui;
 
+const settings = {
+	bgColor: "rgb(65,65,65)",
+	canvasHeight: "window",
+	canvasWidth: "window",
+	container: "js-webgl-container-1",
+	customBoundingBox: null,
+	dotAmount: 80,
+	dotColor: "rgb(0,0,0)",
+	dotSize: 0.2,
+	fogIntencity: 350,
+	opacity: 1,
+	position: {
+		x: 0,
+		y: 0,
+		z: 0,
+	},
+	shapeColor: "rgb(65,65,65)",
+	speed: 0.00001,
+	transformIntencity: 10,
+	transformScale: 0,
+	variant: 0,
+};
+
+const start = Date.now();
+
 function init() {
 	initRenderer();
 	
 	scene = new THREE.Scene();
 	
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-	camera.position.z = 1.5;
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.z = 50;
 	
 	initControls();
 	
 	var light = new THREE.DirectionalLight(0xffffff, 1);
 	light.position.set(5,3,5);
 	scene.add(light);
+
+	// fog
+	const fogColor = 'rgb(65,65,65)';
+  scene.fog = new THREE.Fog(fogColor, 0, settings.fogIntencity);
+	scene.userData.fog = scene.fog;
+
+	const material = materialGeomitry(scene);
+
+	// init sphere with materialoptions
+  const mesh = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(20, 4),
+    material,
+  );
+
+  scene.userData.material = material;
+	scene.userData.material.userData = { speedSettings: settings.speed };
+	
+	scene.add(mesh);
+	mesh.position.set(settings.position.x, settings.position.y, settings.position.z);
+	rotateObject(mesh, -10, 160, 10);
 	
 	window.addEventListener('resize', onResize, false);
 }
@@ -43,6 +88,7 @@ function onResize() {
 
 function render() {
 	controls.update();
+	scene.userData.material.uniforms.time.value = scene.userData.material.userData.speedSettings * (Date.now() - start);
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }
@@ -72,4 +118,76 @@ function initControls() {
 	controls.minDistance = 0;
 	controls.maxDistance = 700;
 	// controls.enablePan = true;
+}
+
+function materialGeomitry(scene) {
+	const material = new THREE.ShaderMaterial({
+    uniforms: {
+      transformScale: {
+        type: 'f',
+        value: settings.transformScale,
+      },
+      transformIntencity: {
+        type: 'f',
+        value: settings.transformIntencity,
+      },
+      opacity: {
+        type: 'f',
+        value: settings.opacity,
+      },
+      radius1: {
+        type: 'f',
+        value: settings.dotSize,
+      },
+      radius2: {
+        type: 'f',
+        value: settings.dotSize,
+      },
+      amount: {
+        type: 'f',
+        value: settings.dotAmount,
+      },
+      time: {
+        type: 'f',
+        value: 0.0,
+      },
+      fogColor: {
+        type: 'c',
+        value: scene.fog.color,
+      },
+      fogNear: {
+        type: 'f',
+        value: scene.fog.near,
+      },
+      fogFar: {
+        type: 'f',
+        value: scene.fog.far,
+      },
+      dotColor: {
+        type: 'c',
+        value: new THREE.Color(settings.dotColor),
+      },
+      shapeColor: {
+        type: 'c',
+        value: new THREE.Color(settings.shapeColor),
+      },
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    fog: true,
+	});
+	
+	const speedFactor = 0.00001;
+  material.uniforms.time.value = speedFactor * (Date.now() - start);
+
+  // enable transparency in the material
+	material.transparent = true;
+
+	return material;
+}
+
+function rotateObject(object, degreeX = 0, degreeY = 0, degreeZ = 0) {
+  object.rotateX(THREE.Math.degToRad(degreeX));
+  object.rotateY(THREE.Math.degToRad(degreeY));
+  object.rotateZ(THREE.Math.degToRad(degreeZ));
 }
